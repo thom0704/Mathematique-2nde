c309e25e5e8de5b071abaade93f838ba247a1980
thom0704 <51696439+thom0704@users.noreply.github.com>
2019-06-18 11:13:59 +0100

    affine

--- main2.js
+++ main2.js
@@ -1,35 +1,8 @@
-// Image switcher code
 
-var myImage = document.querySelector('img');
-
-myImage.onclick = function() {
-  var mySrc = myImage.getAttribute('src');
-  if(mySrc === 'images/firefox-icon.png') {
-    myImage.setAttribute ('src','images/firefox2.png');
-  } else {
-    myImage.setAttribute ('src','images/firefox-icon.png');
-  }
-}
-
-// Personalized welcome message code
-
-var myButton = document.querySelector('button');
-var myHeading = document.querySelector('h1');
-
-function setUserName() {
-  var myName = prompt('Please enter your name.');
-  localStorage.setItem('name', myName);
-  myHeading.innerHTML = 'Mozilla is cool, ' + myName;
-}
-
-if(!localStorage.getItem('name')) {
-  setUserName();
-} else {
-  var storedName = localStorage.getItem('name');
-  myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
-}
-
-myButton.onclick = function() {
-  setUserName();
-  
-}
+ var getA (fonction: getA '11') {return getA = 11} 
+ var getB (fonction: getB '12') {return getB = 12}
+ var getCanvas (fonction: getCanvas '200') {return getCanvas = 200}  
+ var getX (fonction: getX 'getA*getX+getB) (return getX = A*X+B}
+ alert ('getA, getB, getCanvas, getX')
+ 
+    
