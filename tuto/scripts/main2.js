c87b41f338868228b6465d6db18d43b9f15f0b91
thom0704 <51696439+thom0704@users.noreply.github.com>
2019-06-19 09:18:05 +0100

    afinne

--- tuto/scripts/main2.js
+++ tuto/scripts/main2.js
@@ -1,25 +1,30 @@
 // pop up valeur de a, b et x 
 
-     var myButton = document.querySelector('button');
+var myButton = document.querySelector('button');
+
 function getA() {
      var valeurDeA = prompt('valeur de a');
       localStorage.setItem('getA');
 	  (return getA = 11)
+}
 
 function getB() {
      var valeurDeB = prompt('valeur de b');
       localStorage.setItem('getB');
 	  (return getB = 12)
+}
 	  
 function getX() {
      var valeurDeX = prompt('valeur de x');
       localStorage.setItem('getX');
 	  (return getX = 'getA*x+getB' )
+}
 	  
 function getCanvas() {
      var valeurDeCanvas = prompt('valeur de canvas');
       localStorage.setItem('getCanvas');
 	  (return getX = 200 )
+}
 	  
      
   
