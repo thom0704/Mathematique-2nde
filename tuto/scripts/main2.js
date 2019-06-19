6058ee275f04b88fb8f1e9ba2fdd7e60dfd7fa3c
thom0704 <51696439+thom0704@users.noreply.github.com>
2019-06-19 09:02:10 +0100

    affine

--- tuto/scripts/main2.js
+++ tuto/scripts/main2.js
@@ -1,8 +1,10 @@
-var getA (fonction: getA '11') {return getA = 11} 
- var getB (fonction: getB '12') {return getB = 12}
- var getCanvas (fonction: getCanvas '200') {return getCanvas = 200}  
- var getX (fonction: getX 'getA*getX+getB) (return getX = A*X+B}
- var myButton = document.querySelector('button');
-  function setUserName() {
- alert
- localStorage.setItem('name', myName);
+// pop up valeur de a, b et x 
+
+     var myButton = document.querySelector('button');
+function getA() {
+     var valeurDeA = prompt('valeur de a');
+      localStorage.setItem('getA');
+	  (return getA = 11)
+	  
+     
+  
