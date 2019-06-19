c093447c0d2f885326ecbc0442737a6eb45fc4a4
thom0704 <51696439+thom0704@users.noreply.github.com>
2019-06-19 10:50:46 +0100

    affine

--- tuto/scripts/main2.js
+++ tuto/scripts/main2.js
@@ -1,24 +1,31 @@
 // pop up valeur de a, b et x 
 
 var myButton = document.querySelector('button');
+window.alert("");
 
 function getA() {
      return 11;
 var getA = document.querySelector('button')
+var getA = document.querySelector('window.alert')
 }
 
 function getB() {
      return getB = 12
 var getB = document.querySelector('button')
+var getB = document.querySelector('window.alert')
 }
 	  
 function getX() {
      return getX = 13
 var getX = document.querySelector('button')
+var getX = document.querySelector('window.alert')
 }
 	  
 function getCanvas() {
      return getX = 200
-var getCanvas = document.querySelector('button') 
+var getCanvas = document.querySelector('button')
+var getCanvas = document.querySelector('window.alert')
 }
+
+
  
