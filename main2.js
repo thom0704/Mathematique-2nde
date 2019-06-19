019828bf43b7e215af27b30838c50b889bb579c8
thom0704 <51696439+thom0704@users.noreply.github.com>
2019-06-19 08:39:47 +0100

    affine

--- main2.js
+++ main2.js
@@ -3,6 +3,7 @@
  var getB (fonction: getB '12') {return getB = 12}
  var getCanvas (fonction: getCanvas '200') {return getCanvas = 200}  
  var getX (fonction: getX 'getA*getX+getB) (return getX = A*X+B}
- alert ('getA, getB, getCanvas, getX')
- 
-    
+ var myButton = document.querySelector('button');
+  function setUserName() {
+ alert
+ localStorage.setItem('name', myName);
