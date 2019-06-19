6e5fc3c406735975f1f52619c1e506ad319b9aa3
thom0704 <51696439+thom0704@users.noreply.github.com>
2019-06-19 09:07:54 +0100

    affine

--- tuto/scripts/main2.js
+++ tuto/scripts/main2.js
@@ -5,6 +5,21 @@ function getA() {
      var valeurDeA = prompt('valeur de a');
       localStorage.setItem('getA');
 	  (return getA = 11)
+
+function getB() {
+     var valeurDeB = prompt('valeur de b');
+      localStorage.setItem('getB');
+	  (return getB = 12)
+	  
+function getX() {
+     var valeurDeX = prompt('valeur de x');
+      localStorage.setItem('getX');
+	  (return getX = 'getA*x+getB' )
+	  
+function getCanvas() {
+     var valeurDeCanvas = prompt('valeur de canvas');
+      localStorage.setItem('getCanvas');
+	  (return getX = 200 )
 	  
      
   
