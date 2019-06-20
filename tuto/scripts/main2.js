173828574a5ebd8e3f76c71d95e9693fc163f45b
Yohann <1492427+gladdiologist@users.noreply.github.com>
2019-06-20 10:21:40 +0200

    Revert "Done"
    This reverts commit 28da3bf8ce9af1b78caa2f83bf6260dbce1c896f.

--- tuto/scripts/main2.js
+++ tuto/scripts/main2.js
@@ -1,76 +1,27 @@
 // pop up valeur de a, b et x 
 
 var myButton = document.querySelector('button');
-var canvas = document.querySelector('#tutorial');
-var ctx = canvas.getContext("2d");
-var y = [];
-
-myButton.onclick = function() {
-  doIt();
-}
-
-function doIt() {
-  compute();
-  effacer();
-  
-  for (var x = 0; x < canvas.width; x++) {
-    point(x);
-  }
-}
+window.alert("valeur de a,b,x et la taille du canvas");
 
 function getA() {
-  return getCoefficient("a");
+     var getA = document.querySelector('button')
+return 11;
 }
 
 function getB() {
-  return getCoefficient("b");
-}
-
-function getCoefficient(coef) {
-  var coef = document.querySelector('#'+coef)
-  return +coef.value;
+     var getB = document.querySelector('button')
+return 12;
 }
 	  
-
-function compute() {
-  
-  y = [];
-  
-  for (var x = 0; x < canvas.width; x++) {
-    let result = affine(getA(),getB(),x);
-    y.push(result);
-  }
+function getX() {
+    var getX = document.querySelector('button')
+ return 13;
 }
-
-function affine(a,b,x) {
-  return a*x + b;
+	  
+function getCanvas() {
+    var getCanvas = document.querySelector('button')
+ return 200;
 }
-
-function point(x) {
-  
-  ctx.fillRect(x,translate(y[x],Math.max(...y)),1,1);
-}
-
-function translate(y,max) {
-  return canvas.height - y*canvas.height/max;
-}
-
-function effacer() {
-  ctx.fillStyle = "#fff";
-  ctx.fillRect(0,0,canvas.width,canvas.height);
-  ctx.fillStyle = "#568745";
-}
-
-function animation(t) {
-  console.log(t);
-  let pente = Math.random() > 0.5 ? 1 : -1;
-  document.querySelector('#a').value = Math.random() * 1000 * pente;
-  document.querySelector('#b').value = Math.random() * 10000;
-  doIt();
-  requestAnimationFrame(animation);
-}
-
-requestAnimationFrame(animation);
 
 
  
