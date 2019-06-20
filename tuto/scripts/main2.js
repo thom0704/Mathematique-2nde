9c16359379df476177765a222e9debc89702e518
Yohann <1492427+gladdiologist@users.noreply.github.com>
2019-06-20 10:23:02 +0200

    Revert "Revert "Done""
    This reverts commit 173828574a5ebd8e3f76c71d95e9693fc163f45b.

--- tuto/scripts/main2.js
+++ tuto/scripts/main2.js
@@ -1,27 +1,76 @@
 // pop up valeur de a, b et x 
 
 var myButton = document.querySelector('button');
-window.alert("valeur de a,b,x et la taille du canvas");
+var canvas = document.querySelector('#tutorial');
+var ctx = canvas.getContext("2d");
+var y = [];
+
+myButton.onclick = function() {
+  doIt();
+}
+
+function doIt() {
+  compute();
+  effacer();
+  
+  for (var x = 0; x < canvas.width; x++) {
+    point(x);
+  }
+}
 
 function getA() {
-     var getA = document.querySelector('button')
-return 11;
+  return getCoefficient("a");
 }
 
 function getB() {
-     var getB = document.querySelector('button')
-return 12;
+  return getCoefficient("b");
+}
+
+function getCoefficient(coef) {
+  var coef = document.querySelector('#'+coef)
+  return +coef.value;
 }
 	  
-function getX() {
-    var getX = document.querySelector('button')
- return 13;
+
+function compute() {
+  
+  y = [];
+  
+  for (var x = 0; x < canvas.width; x++) {
+    let result = affine(getA(),getB(),x);
+    y.push(result);
+  }
 }
-	  
-function getCanvas() {
-    var getCanvas = document.querySelector('button')
- return 200;
+
+function affine(a,b,x) {
+  return a*x + b;
 }
+
+function point(x) {
+  
+  ctx.fillRect(x,translate(y[x],Math.max(...y)),1,1);
+}
+
+function translate(y,max) {
+  return canvas.height - y*canvas.height/max;
+}
+
+function effacer() {
+  ctx.fillStyle = "#fff";
+  ctx.fillRect(0,0,canvas.width,canvas.height);
+  ctx.fillStyle = "#568745";
+}
+
+function animation(t) {
+  console.log(t);
+  let pente = Math.random() > 0.5 ? 1 : -1;
+  document.querySelector('#a').value = Math.random() * 1000 * pente;
+  document.querySelector('#b').value = Math.random() * 10000;
+  doIt();
+  requestAnimationFrame(animation);
+}
+
+requestAnimationFrame(animation);
 
 
  
